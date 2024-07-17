093dc0ae1cf3f40293c105a0068f26cb91e8ac49
japwin <japwin.entertainment@gmail.com>
2024-07-17 22:42:58 +0100

    added successful app

--- no mo.py
+++ no mo.py
@@ -10,8 +10,9 @@ class BoxingMatchApp:
         self.fighter2 = ""
         self.SCard1 = []
         self.SCard2 = []
+        self.winner = ""
         self.current_round = 1
-        self.round_instances = {}
+        self.round_instance = None
 
         self.create_widgets()
 
@@ -26,45 +27,44 @@ class BoxingMatchApp:
 
         begin_button = tk.Button(self.master, text="Begin", command=self.start_match)
         begin_button.grid(row=2, columnspan=2)
+        self.master.bind('<Return>', lambda event: self.start_match())  # Bind Enter key
 
     def start_match(self):
         self.fighter1 = self.entry_fighter1.get()
         self.fighter2 = self.entry_fighter2.get()
 
-        self.round_instances[1] = Round(self.master, self.fighter1, self.fighter2, self.SCard1, self.SCard2, self.next_round)
-        self.round_instances[1].start_round()
+        self.round_instance = Round(self.master, self.fighter1, self.fighter2, self.SCard1, self.SCard2, self.current_round, self.next_round, self.set_winner)
+        self.round_instance.start_round()
 
     def next_round(self):
         self.current_round += 1
         if self.current_round <= 15:
-            if self.current_round == 15:
-                self.round_instances[self.current_round] = Round(self.master, self.fighter1, self.fighter2, self.SCard1, self.SCard2, self.display_results)
-                self.round_instances[self.current_round].start_round()
-            else:
-                self.round_instances[self.current_round] = Round(self.master, self.fighter1, self.fighter2, self.SCard1, self.SCard2, self.next_round)
-                self.round_instances[self.current_round].start_round()
+            self.round_instance.clear_widgets()
+            self.round_instance = Round(self.master, self.fighter1, self.fighter2, self.SCard1, self.SCard2, self.current_round, self.next_round, self.set_winner)
+            self.round_instance.start_round()
         else:
-            messagebox.showinfo("End of Rounds", "All rounds completed.")
             self.display_results()
 
+    def set_winner(self, winner):
+        self.winner = winner
+        self.display_results()
+
     def display_results(self):
-        self.results = Results(self.master, self.fighter1, self.fighter2, self.SCard1, self.SCard2)
+        self.round_instance.clear_widgets()
+        self.results = Results(self.master, self.fighter1, self.fighter2, self.SCard1, self.SCard2, self.winner)
 
 class Round:
-    def __init__(self, master, fighter1, fighter2, SCard1, SCard2, next_round_callback):
+    def __init__(self, master, fighter1, fighter2, SCard1, SCard2, round_number, next_round_callback, set_winner_callback):
         self.master = master
         self.fighter1 = fighter1
         self.fighter2 = fighter2
         self.SCard1 = SCard1
         self.SCard2 = SCard2
+        self.round_number = round_number
         self.next_round_callback = next_round_callback
-        self.round_number = None
-
-        self.create_widgets()
+        self.set_winner_callback = set_winner_callback
 
     def create_widgets(self):
-        self.round_number = len(self.SCard1) // 1 + 1
-
         tk.Label(self.master, text=f"Round {self.round_number}").grid(row=3, columnspan=2)
         tk.Label(self.master, text=f"{self.fighter1} vs {self.fighter2}").grid(row=4, columnspan=2)
 
@@ -78,59 +78,113 @@ class Round:
 
         quit_button = tk.Button(self.master, text="Quit", command=self.master.quit)
         quit_button.grid(row=7, column=0)
+        self.master.bind('<Return>', lambda event: self.master.quit())  # Bind Enter key to Quit
 
         continue_button = tk.Button(self.master, text="Continue", command=self.submit_scores)
         continue_button.grid(row=7, column=1)
+        self.master.bind('<Return>', lambda event: self.submit_scores())  # Bind Enter key to Continue
+
+        ko_button = tk.Button(self.master, text="KO", command=self.ko)
+        ko_button.grid(row=8, column=0)
+        self.master.bind('<Return>', lambda event: self.ko())  # Bind Enter key to KO
+
+        no_contest_button = tk.Button(self.master, text="No Contest", command=self.no_contest)
+        no_contest_button.grid(row=8, column=1)
+        self.master.bind('<Return>', lambda event: self.no_contest())  # Bind Enter key to No Contest
+
+        points_button = tk.Button(self.master, text="Points", command=self.submit_scores)
+        points_button.grid(row=8, column=2)
+        self.master.bind('<Return>', lambda event: self.submit_scores())  # Bind Enter key to Points
 
     def submit_scores(self):
-        round_score1 = int(self.entry_fighter1.get())
-        round_score2 = int(self.entry_fighter2.get())
+        round_score1 = self.entry_fighter1.get()
+        round_score2 = self.entry_fighter2.get()
 
-        self.SCard1.append(round_score1)
-        self.SCard2.append(round_score2)
-
-        overall1 = sum(self.SCard1)
-        overall2 = sum(self.SCard2)
-
-        print(f"{self.fighter1}: {self.SCard1} - Total: {overall1}")
-        print(f"{self.fighter2}: {self.SCard2} - Total: {overall2}")
+        if round_score1.isdigit() and round_score2.isdigit():
+            round_score1 = int(round_score1)
+            round_score2 = int(round_score2)
+            self.SCard1.append(round_score1)
+            self.SCard2.append(round_score2)
+        else:
+            messagebox.showerror("Invalid Input", "Scores must be integers")
+            return
 
         self.next_round_callback()
 
+    def ko(self):
+        self.ko_popup = tk.Toplevel(self.master)
+        self.ko_popup.title("KO Winner")
+
+        tk.Label(self.ko_popup, text="Who won by KO?").pack()
+
+        self.ko_winner_choice = tk.StringVar(self.ko_popup)
+        self.ko_winner_choice.set(self.fighter1)  # Default choice
+
+        ko_winner_menu = tk.OptionMenu(self.ko_popup, self.ko_winner_choice, self.fighter1, self.fighter2)
+        ko_winner_menu.pack()
+
+        ko_confirm_button = tk.Button(self.ko_popup, text="Confirm", command=self.ko_confirm)
+        ko_confirm_button.pack()
+        self.ko_popup.bind('<Return>', lambda event: self.ko_confirm())  # Bind Enter key to Confirm
+
+    def ko_confirm(self):
+        winner = self.ko_winner_choice.get()
+        if winner == self.fighter1:
+            self.SCard1.append("KO")
+            self.SCard2.append("Loss")
+        elif winner == self.fighter2:
+            self.SCard2.append("KO")
+            self.SCard1.append("Loss")
+
+        self.ko_popup.destroy()
+        self.set_winner_callback(f"{winner} by KO")
+
+    def no_contest(self):
+        self.SCard1.append("No Contest")
+        self.SCard2.append("No Contest")
+        self.set_winner_callback("No Contest")
+
     def start_round(self):
-        self.round_number = len(self.SCard1) // 2 + 1
+        self.create_widgets()
+
+    def clear_widgets(self):
+        for widget in self.master.winfo_children():
+            widget.grid_forget()
 
 class Results:
-    def __init__(self, master, fighter1, fighter2, SCard1, SCard2):
+    def __init__(self, master, fighter1, fighter2, SCard1, SCard2, winner):
         self.master = master
         self.fighter1 = fighter1
         self.fighter2 = fighter2
         self.SCard1 = SCard1
         self.SCard2 = SCard2
+        self.winner = winner
 
         self.show_results()
 
     def show_results(self):
-        tk.Label(self.master, text="Results").grid(row=8, columnspan=2)
+        tk.Label(self.master, text="Results").grid(row=0, columnspan=2)
         
-        overall1 = sum(self.SCard1)
-        overall2 = sum(self.SCard2)
+        overall1 = sum([score for score in self.SCard1 if isinstance(score, int)])
+        overall2 = sum([score for score in self.SCard2 if isinstance(score, int)])
 
-        if overall1 > overall2:
-            winner = self.fighter1
-        elif overall2 > overall1:
-            winner = self.fighter2
-        else:
-            winner = "Draw"
+        if self.winner == "":
+            if overall1 > overall2:
+                self.winner = self.fighter1
+            elif overall2 > overall1:
+                self.winner = self.fighter2
+            else:
+                self.winner = "Draw"
 
-        result_label = tk.Label(self.master, text=f"Winner: {winner}")
-        result_label.grid(row=9, columnspan=2)
+        result_label = tk.Label(self.master, text=f"Winner: {self.winner}")
+        result_label.grid(row=1, columnspan=2)
 
-        tk.Label(self.master, text=f"{self.fighter1}: {self.SCard1} - Total: {overall1}").grid(row=10, columnspan=2)
-        tk.Label(self.master, text=f"{self.fighter2}: {self.SCard2} - Total: {overall2}").grid(row=11, columnspan=2)
+        tk.Label(self.master, text=f"{self.fighter1}: {self.SCard1} - Total: {overall1}").grid(row=2, columnspan=2)
+        tk.Label(self.master, text=f"{self.fighter2}: {self.SCard2} - Total: {overall2}").grid(row=3, columnspan=2)
 
         finish_button = tk.Button(self.master, text="Finish", command=self.master.quit)
-        finish_button.grid(row=12, columnspan=2)
+        finish_button.grid(row=4, columnspan=2)
+        self.master.bind('<Return>', lambda event: self.master.quit())  # Bind Enter key to Finish
 
 def main():
     root = tk.Tk()
